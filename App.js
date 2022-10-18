3d52700cfbbe8a4412c794db3a7f8f13450d94dc
jngnandan <jngnandan@gmail.com>
2022-10-18 23:18:09 +0100

    comments section added

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+
   return (
     <View style={styles.container}>
-      <Text style={{fontSize: 26, fontWeight: '600'}}>Comments</Text>
-      
+      <Text style={{fontSize: 36, fontWeight: '600',}}>Comments</Text>
+      <Image source={{uri:'https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png'}} style={{height: 200, width: 300, marginVertical: 20}} />
+      <Text>Say something about 4.0 Technologies</Text>
+      <TextInput placeholder='Your Name' style={{paddingLeft: 4, height: 40, borderColor: 'gray', borderWidth: 1, width: 300, marginTop: 10}} />
+      <TextInput
+        multiline={true}
+          numberOfLines={4}
+          style={{paddingLeft: 4, height: 100, borderColor: 'gray', borderWidth: 1, width: 300, marginTop: 10}}
+          placeholder='Your Comment'
+          />
+          <TouchableOpacity style={{backgroundColor: 'blue', width: 300, height: 40, marginTop: 10, alignItems: 'center', justifyContent: 'center'}}>
+            <Text style={{color: 'white'}}>Submit</Text>
+          </TouchableOpacity>
     </View>
   );
 }
@@ -16,5 +29,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 12,
   },
 });
